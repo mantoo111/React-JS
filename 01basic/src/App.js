603d1b749f1ed75410af5982106700405acc4df6
Ansa Abid <mantoo0201@gmail.com>
2025-02-01 14:35:35 +0500

    React Basics

---
+++ 01basic/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1>lets learn  React.js</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+import React,{useState} from "react"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>React with vite</h1>
+  )
+}
+
+export default App
